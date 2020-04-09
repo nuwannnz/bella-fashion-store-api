06707780a0f4d3080564225759666a885b544bea
Nuwan Karunarathna <nuwankarunarathna@hotmail.com>
2020-04-09 20:56:56 +0530

    [SETUP] Added env file
    Because:
    - We have to extract uri, usernames and passwords from code
    
    I have added the dotenv npm package so after clonning this commit
    you will have to run a `npm install`

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const { API_PREFIX } = require("./config");
 const { registerMiddleware, routes } = require("./api");
--- src/config/index.js
+++ src/config/index.js
@@ -2,24 +2,22 @@ const appRoot = require("app-root-path");
 
 module.exports = {
   database: {
-    uri:
-      process.env.DB_URI ||
-      "mongodb://<dbuser>:<dbpassword>@ds115353.mlab.com:15353/bella-fashion-store",
-    username: process.env.DB_USERNAME || "bella",
-    password: process.env.DB_PASSWORD || "bella@1234"
+    uri: process.env.DB_URI || "localhost",
+    username: process.env.DB_USERNAME || "test",
+    password: process.env.DB_PASSWORD || "test",
   },
 
   env: process.env.NODE_ENV || "development",
 
   logger: {
     errorFilePath: `${appRoot}/logs/error.log`,
-    combinedFilePath: `${appRoot}/logs/combined.log`
+    combinedFilePath: `${appRoot}/logs/combined.log`,
   },
 
   jwt: {
-    secret: process.env.JWT_SEC || "combellaapi"
+    secret: process.env.JWT_SEC || "combellaapi",
   },
 
   PORT: process.env.PORT || 5000,
-  API_PREFIX: "/api/v1"
+  API_PREFIX: "/api/v1",
 };
